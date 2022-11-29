c69df6177854af4175a582a301faa49e161687ea
Leia Soares <leia13soares@outlook.com>
2022-11-29 11:15:10 -0300

    Sap-ui5

---
+++ Component.js
@@ -0,0 +1,33 @@
+//contem a configuração da aplicação 
+sap.ui.define([
+    "sap/ui/core/UIComponent", //Nosso componente herda da classe base sap.ui.core.UIComponent
+    "sap/ui/model/json/JSONModel"
+    
+
+ ], function (UIComponent, JSONModel) {
+    "use strict";
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+        metadata : {
+            interfaces: ["sap.ui.core.IAsyncContentCreation"],
+            manifest: "json"
+      },
+       //A função init do componente é invocada automaticamente pelo OpenUI5 quando o componente é instanciado.
+       init : function () {
+          
+          UIComponent.prototype.init.apply(this, arguments);
+          
+          var oData = {
+             recipient : {
+                name : "Escreva aqui"
+             }
+          };
+          var oModel = new JSONModel(oData);
+          this.setModel(oModel);
+
+          //oque foi mudado
+          this.getRouter().initialize();
+ 
+        }
+    });
+ });
+ 
